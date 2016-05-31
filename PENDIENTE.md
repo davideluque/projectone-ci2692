a659df9b6062214191abee253ced63f181c1ffb2
David Cabeza <david@David.DavidC>
2016-05-31 03:00:28 -0400

    Major changes Translate method 100%

--- PENDIENTE.md
+++ PENDIENTE.md
@@ -5,18 +5,22 @@ Pendiente
 
 <b>*Clase ADNDoble:*</b>
 + Método buscar debería retornar la posición inicial en la que se encuentra la secuencia.
++ En zip arreglar el procedimiento con get_complement
++ En mitosis usar la función zip
++ Cerrar el archivo después de guardarlo y colocar un salto de línea
 + Llamar a unzip sin haber llamado a zip no genera nada ¿Excepciones?
 
 <b>*Clase ARNt:*</b>
-+ Crear un método para complementar el ARNt o modificar los existentes para que detecten si se está en presencia de URACILO o de TIMINA
-
+DONE ~~Crear un método para complementar el ARNt o modificar los existentes para que detecten si se está en presencia de URACILO o de TIMINA~~
++ Todo lo que se consigue antes de un inicio se cataloga como BASURA
 ___
 
 Dudas
 ------
-
 + Clase "proteina"
 + ¿Qué significa que una cadena o proteína sea un objeto de una clase? ¿Instancia?
 + ¿A qué se refiere con definir las clases contenedoras?
 + ¿En el método escribir cuáles son "todas" las cadenas o proteínas que se almacenan?
 + El método traducir traduce el ARN de transporte de una de sus instancias? Es decir que las instancias de traducir son solo ARNts.
++ En el apartado 4. es heapsort o buildmaxheap ?
++ Se ordenan los codones por su frecuencia decrecientemente cada vez que se introduce uno o al final?
--- dna.py
+++ dna.py
@@ -1,37 +1,124 @@
 # Universidad Simón Bolívar
 # CI2692: Laboratory of Algorithms and Structures II
-
 # Project 1: Implementing of algorithms to emulate the synthesis process of 
 # 			 proteins.
 # Authors: David Cabeza 13-10191, Fabiola Martinez 13-10838
-# Last edit: Sun 22, May 2016
-import sys
+# Last edit: Mon 30, May 2016
 
-def get_compliment(sequence):
-	compliment = ""
+import sys
+from random import randint
+
+################### get_complement_tRNA METHOD #################################
+# Description: This method receives a tRNA sequence and complements it changing
+#			   Uracil for Adenine, Guanine for Cytosine and vice versa.
+# Input: Receives a sequence of tRNA to be complemented 
+# Output: Returns the complement of the given tRNA sequence
+################################################################################
+def get_complement_tRNA(sequence):
+	complement = ""
+	for base in sequence:
+		if base == "A":
+			complement += "U"
+		elif base == "G":
+			complement += "C"
+		elif base == "U":
+			complement += "A"
+		else:
+			complement += "G"
+
+	return complement
+
+######################## get_complement METHOD #################################
+# Description: This method receives a DNA sequence and complements it changing
+#			   Timine for Adenine, Guanine for Cytosine and vice versa.
+# Input: Receives a sequence of DNA to be complemented 
+# Output: Returns the complement of the given DNA sequence
+################################################################################
+def get_complement(sequence):
+	complement = ""
 	for nucleobase in sequence:
 		if nucleobase == "A":
-			compliment += "T"
-		elif nucleobase == "T" or nucleobase == 'U':
-			compliment += "A"
+			complement += "T"
+		elif nucleobase == "T":
+			complement += "A"
 		elif nucleobase == "G":
-			compliment += "C"
+			complement += "C"
 		elif nucleobase == "C":
-			compliment += "G"
+			complement += "G"
 
-	return compliment
+	return complement
 
-class ADNDoble(object):
-	"""Class whose instances are sequences of simple ADN. This class manipulates 
+def Partition(A, p, r):
+	x = len(A[r])
+	i = p - 1
+	# Invariant
+	assert(all(len(A[i]) >= x for i in range(0, i+1)))
+	for j in range(p, r):
+		if len(A[j]) > x:
+			i += 1
+			A[i], A[j] = A[j], A[i]
+		elif len(A[j]) == x:
+			if min(A[j]) <= min(A[r]):
+				i+= 1
+				A[i], A[j] = A[j], A[i]
+		# Invariant
+		assert(all(len(A[i]) >= x for i in range(0, i+1)))
+	A[i+1], A[r] = A[r], A[i+1]
+	return i + 1
+
+def quicksort_randomizedPartition(A, p, r):
+	i = randint(p, r)
+	A[i], A[r] = A[r], A[i]
+	return Partition(A, p, r)
+
+def quicksort(A, p = None, r = None):
+	p = p if p else 0 
+	r = r if r else len(A) - 1
+
+	if p < r:
+		q = quicksort_randomizedPartition(A, p, r)
+		quicksort(A, p, q-1)
+		quicksort(A, q+1, r)
+
+	return A
+
+def heapsort(A):
+	n = len(A)	
+	buildHeap(A)
+
+	for i in range(n-1, 0, -1):
+		A[0], A[i] = A[i], A[0]
+		heapify(A, 0, i)
+
+def buildHeap(A):
+	n = len(A)
+	for i in range((n//2), -1, -1):
+		heapify(A, i, n)
+
+def heapify(A, idx, mx):
+	largest = idx
+	left = (2*idx) + 1
+	right = (2*idx) + 2
+
+	if left < mx and A[left][1] < A[idx][1]:
+		largest = left
+	if right < mx and A[right][1] < A[largest][1]:
+		largest = right
+	if largest != idx:
+		A[idx], A[largest] = A[largest], A[idx]
+		heapify(A, largest, mx)
+
+class DNADoble(object):
+	"""Class whose instances are sequences of simple DNA. This class manipulates 
 	its associated double sequence"""
 	def __init__(self, simpleseq):
 		self.simpleseq = simpleseq.upper()
 		self.sseqlenght = len(self.simpleseq)
-		self.compliment = ""
+		self.complement = ""
 		self.doubleseq = ""
 
 	def zip(self):
-		print('\nBeginning to zip the sequence', ADN.simpleseq)
+		print('\nBeginning to zip the sequence', DNA.simpleseq)
 		for nucleobase in self.simpleseq:
 			if nucleobase == "A":
 				self.doubleseq += "AT"
@@ -42,13 +129,13 @@ class ADNDoble(object):
 			elif nucleobase == "C":
 				self.doubleseq += "CG"
 
-		self.compliment = get_compliment(self.simpleseq)
-		print('The compliment for the DNA sequence given is:', self.compliment)
+		self.complement = get_complement(self.simpleseq)
+		print('The complement for the DNA sequence given is:', self.complement)
 		print('The double sequence for the given is:', self.doubleseq)
 		print('Zip process was made successfully!!')
 	
 	def unzip(self):
-		print('\nBeginning to unzip the sequence', ADN.doubleseq)	
+		print('\nBeginning to unzip the sequence', DNA.doubleseq)	
 		seqOne = ""
 		for i in range(0, len(self.doubleseq), 2):
 			seqOne += self.doubleseq[i]
@@ -59,7 +146,7 @@ class ADNDoble(object):
 		print(seqOne,'and', seqTwo)
 
 	def mitosis(self):
-		print('\nBeginning the mitosis process for the sequence', ADN.doubleseq)	
+		print('\nBeginning the mitosis process for the sequence', DNA.doubleseq)	
 		print('Beginning to unzip the double sequence')
 		seqOne = ""
 		for i in range(0, len(self.doubleseq), 2):
@@ -67,17 +154,17 @@ class ADNDoble(object):
 		seqTwo = ""
 		for j in range(1, len(self.doubleseq), 2):
 			seqTwo += self.doubleseq[j]
-		print('Beginning to calculate the compliments')
-		CseqOne = get_compliment(seqOne)
-		CseqTwo = get_compliment(seqTwo)
+		print('Beginning to calculate the complements')
+		CseqOne = get_complement(seqOne)
+		CseqTwo = get_complement(seqTwo)
 
 	def buscar(self, subcadena):
 		print("\nBeginning search subsequence process..")
-		compliment = get_compliment(subcadena)
+		complement = get_complement(subcadena)
 		if subcadena in self.doubleseq:
 			print('MATCH, we found', subcadena, 'in', self.doubleseq)
-		if compliment in self.doubleseq:
-			print('MATCH, we found the compliment', compliment, 'of', subcadena,'in', self.doubleseq)
+		if complement in self.doubleseq:
+			print('MATCH, we found the complement', complement, 'of', subcadena,'in', self.doubleseq)
 
 	def imprimir(self):
 		print()
@@ -91,41 +178,44 @@ class ADNDoble(object):
 		with open(file, 'a') as f:
 			f.write(self.doubleseq)
 
-class ADNSimple(object):
-	"""Instances: Simple ADN sequence"""
+class DNASimple(object):
+	"""Instances: Simple DNA sequence"""
 	def __init__(self, sequence):
 		self.sequence = sequence
-		self.complim = ""
+		self.complem = ""
 
-	def compliment(self):
+	def complement(self):
 		for nucleobase in self.sequence:
 			if nucleobase == "A":
-				self.complim += "T"
+				self.complem += "T"
 			elif nucleobase == "T":
-				self.complim += "A"
+				self.complem += "A"
 			elif nucleobase == "G":
-				self.complim += "C"
+				self.complem += "C"
 			elif nucleobase == "C":
-				self.complim += "G"
+				self.complem += "G"
 
 	def transliterate(self):
-		if not self.complim:
-			self.complim == self.compliment()
+		if not self.complem:
+			self.complem == self.complement()
 
-		self.ARNt = ""
-		for nb in self.complim:
+		self.tRNA = ""
+		for nb in self.complem:
 			if nb == "T":
-				self.ARNt += "U"
+				self.tRNA += "U"
 			else:
-				self.ARNt += nb
+				self.tRNA += nb
 
-		return self.ARNt
+		return self.tRNA
 
-class ARNt(ADNSimple):
-	"""docstring for ARNt"""
+class tRNA():
+	"""docstring for tRNA"""
 	def __init__(self, sequence):
 		self.sequence = sequence
-		self.size = len(sequence)
+		self.size = len(self.sequence)
+		self.complement = ""
+		self.complementsize = len(self.complement)
+		self.bases = ['U', 'C', 'A', 'G']
 		self.aminoacids = ['Phe', 'Phe', 'Leu','Leu', 'Ser', 'Ser', 'Ser', 
 		'Ser', 'Tyr', 'Tyr', 'STOP', 'STOP', 'Cys', 'Cys', 'STOP', 'Trp',
 		'Leu', 'Leu','Leu','Leu', 'Pro', 'Pro', 'Pro', 'Pro', 'His', 'His',
@@ -134,66 +224,84 @@ class ARNt(ADNSimple):
 		'Arg', 'Arg', 'Val', 'Val', 'Val', 'Val', 'Ala', 'Ala', 'Ala', 'Ala', 
 		'Asp', 'Asp', 'Glu', 'Glu', 'Gly', 'Gly', 'Gly', 'Gly']
 		self.proteins = []
-		self.letras = ['U', 'C', 'A', 'G']
-		self.ARNJunks = []
+		self.tRNATrash = []
+		self.DNATrash = []
+		self.frequencies = []
 
-	def translate(self):
-		start = False
+	#################### Translate METHOD ####################################
+	# Description: This method translates a sequence into proteins, checks if
+	# exists 
+	##########################################################################
+	def Translate(self):
+		self.complement = get_complement_tRNA(self.sequence)
+		# Cambiar todos los self.sequence y self.size por los del complemento
+		#print(self.sequence)
+		Start = False
+		End = True
 		for i in range(0, self.size, 3):
-			trio = self.sequence[i]+self.sequence[i+1]+self.sequence[i+2]
-			if i == 69:
-				print(trio)
-				print(start)
-			letras = False
-			if not start and trio == 'AUG':
-				start = True
-				start_index = i
-				end = False
-				letras = True
-				temp = []
-			if start and trio != 'AUG' and trio != 'UAA' and trio != 'UAG'\
-			and trio != 'UGA':
-				value = self.letras.index(self.sequence[i]) * 16 \
-				+ self.letras.index(self.sequence[i+1]) * 4 \
-				+ self.letras.index(self.sequence[i+2]) * 1
-				temp.append(self.aminoacids[value])
-				letras = True
-			if not end and trio == 'UAA' or trio == 'UAG' or trio == 'UGA':	
-				self.proteins.append(temp)
-				del temp
-				start = False
-				end = True
-				letras = True
-			if start and not end and not letras:
-				j = start_index + 3
-				temporary = []
-				while j != i:
-					junk = self.sequence[j] + self.sequence[j+1] + self.sequence[j+2]
-					j += 3
-					temporary.append(junk)
-				self.ARNJunks.append(temporary)
-				del temporary
-				start = False
-				end = True
-				del temp
-		
-		print(self.proteins)
-		print(self.ARNJunks)
+			Trio = self.sequence[i] + self.sequence[i+1] + self.sequence[i+2]
+			if not self.frequencies:
+				self.frequencies.append([Trio, 1])
+			else:
+				j = 0
+				while j < len(self.frequencies):
+					if Trio == self.frequencies[j][0]: 
+						self.frequencies[j][1] += 1
+						break
+					j += 1
+				else:	
+					self.frequencies.append([Trio, 1])
+			if Trio == 'AUG' and not Start:
+				Start = True
+				Start_index = i
+				End = False
+				Temp_proteins = []
+				continue
+			if Start and Trio != 'UAA' and Trio != 'UAG' and Trio != 'UGA':
+					Value = self.bases.index(self.sequence[i]) * 16 \
+					+ self.bases.index(self.sequence[i+1]) * 4 \
+					+ self.bases.index(self.sequence[i+2]) * 1
+					Temp_proteins.append(self.aminoacids[Value])
+					continue
+			if Trio == 'UAA' or Trio == 'UAG' or Trio == 'UGA':
+				self.proteins.append(Temp_proteins)
+				del Temp_proteins
+				Start = False
+				End = True
+				continue
+		else:
+			if Start and not End:
+				#Start_index += 3 # If this is commented, the AUG will not take into
+				# account for trash.
+				Trash = ''
+				while Start_index != i+3:
+					Temp_trash = self.sequence[Start_index] + self.sequence[Start_index+1] \
+					+ self.sequence[Start_index+2]
+					Trash += Temp_trash
+					Start_index += 3
+				self.DNATrash.append(get_complement(Trash))
+				self.tRNATrash.append(Trash)
+
+		heapsort(self.frequencies)
+		quicksort(self.proteins)
+		#print(self.frequencies)
+		#print(self.proteins)
+		#print(self.tRNATrash)
+		#print(self.DNATrash)
 
 # Main program			
 if __name__ == '__main__':
-	ADN = ADNDoble("ATGGGCAATCGGTTTGC")
-	ADN.zip()
-	ADN.unzip()
-	ADN.buscar("ATA")
-	ADN.mitosis()
-	ADN.imprimir()
+	DNA = DNADoble("ATGGGCAATCGGTTTGC") 
+	DNA.zip()
+	DNA.unzip()
+	DNA.buscar("ATA")
+	DNA.mitosis()
+	DNA.imprimir()
 
-	var = get_compliment('ATGTTTTTCTTATTGTCTTCCTCATCGTATTACTAAATGACGATAGTAGATTGAATGTTCTAAATGTTTATGTCTTAA')
-	ADNs = ADNSimple(var)
-	ADNs.compliment() # If you don't call this method first, the transliterate method
-	transliterated = ADNs.transliterate() # will do it automatically.
+	var = get_complement("ATGTTTTTCTTATTGTCTTCCTCATCGTATTACTAAATGACGATAGTAGATTGAATGTTCTAAATGTTTATGTCTTAAATGCTTAACTGAATGTTCAGCTAGATGGAGTAT")
+	DNAs = DNASimple(var)
+	DNAs.complement() # If you don't call this method first, the transliterate method
+	transliterated = DNAs.transliterate() # will do it automatically.
 
-	print(transliterated)
-	protein = ARNt(transliterated)
-	protein.translate()
+	protein = tRNA(transliterated)
+	protein.Translate()
